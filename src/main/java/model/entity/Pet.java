6f4407a5b7cb279b755d9cde0a5a7a16110fac63
Ivo Ramos Junior <ivo.ramos.94@gmail.com>
2022-05-30 08:21:11 -0300

    Criado Enum TipoAnimal. Mapeado a classe Pet

--- src/main/java/model/entity/Pet.java
+++ src/main/java/model/entity/Pet.java
@@ -1,19 +1,87 @@
 package model.entity;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
+
+import model.enums.TipoAnimal;
 
 @Entity
 public class Pet {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID_PET")
 	private Long idPet;
 	
-	@NotBlank(message = "O nome não pode ser vazio e nem estar em branco")
 	private String nome;
+	
+	@Column(name = "DT_NASCIMENTO")
+	private LocalDate dataNascimento;
+	
+	@Column(name = "NOME_DONO")
+	private String nomeDono;
+	
+	private String raca;
+	
+	@Enumerated(EnumType.STRING)
+	private TipoAnimal tipoAnimal;
+
+	public Pet() {
+	}
+
+	public Long getIdPet() {
+		return idPet;
+	}
+
+	public void setIdPet(Long idPet) {
+		this.idPet = idPet;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNomeDono() {
+		return nomeDono;
+	}
+
+	public void setNomeDono(String nomeDono) {
+		this.nomeDono = nomeDono;
+	}
+
+	public String getRaca() {
+		return raca;
+	}
+
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+
+	public TipoAnimal getTipoAnimal() {
+		return tipoAnimal;
+	}
+
+	public void setTipoAnimal(TipoAnimal tipoAnimal) {
+		this.tipoAnimal = tipoAnimal;
+	}
 
 }
