95a41d6698deeb45597d779cd99d999d4ebded56
LEONARDO GARCIA DE OLIVEIRA <leonardo.oliveira7@alunos.sc.senac.br>
2022-06-02 11:12:42 -0300

    criada primeira estrutura da tela

---
+++ src/main/java/petshop/views/TelaRegistrarAtendimento.java
@@ -0,0 +1,10 @@
+package petshop.views;
+
+import javax.swing.*;
+
+public class TelaRegistrarAtendimento extends JPanel {
+    private JComboBox atendimentosComboBox;
+    private JComboBox servicosComboBox;
+    private JCheckBox checkBox1;
+    private JButton registrarButton;
+}
