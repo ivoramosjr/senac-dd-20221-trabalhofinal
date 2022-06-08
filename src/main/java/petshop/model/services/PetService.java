d26b0776968494a4c9438d39b6d26ab0eff2bbcc
Ivo Ramos Junior <ivo.ramos.94@gmail.com>
2022-06-08 15:58:15 -0300

    Criado o PetService corrigido

---
+++ src/main/java/petshop/model/services/PetService.java
@@ -0,0 +1,35 @@
+package petshop.model.services;
+
+import petshop.exceptions.AtributosInvalidosException;
+import petshop.exceptions.RegistroNaoEncontradoException;
+import petshop.model.business.PetBusiness;
+import petshop.model.dtos.FiltroPetDTO;
+import petshop.model.dtos.PetDTO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class PetService {
+
+    private PetBusiness petBusiness;
+
+    public PetService(){
+        petBusiness = new PetBusiness();
+    }
+
+    public void save(PetDTO petDTO) throws SQLException, AtributosInvalidosException {
+        petBusiness.save(petDTO);
+    }
+
+    public void update(Long idPet, PetDTO petDTO) throws RegistroNaoEncontradoException {
+        petBusiness.update(idPet, petDTO);
+    }
+
+    public List<PetDTO> listAll(){
+        return petBusiness.listAll();
+    }
+
+    public List<PetDTO> findWithFilter(FiltroPetDTO filtro){
+        return petBusiness.findWithFilter(filtro);
+    }
+}
